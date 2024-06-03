5782db1359f196bd2ca7b12be5ad5074f0f41b46
YanaZhekova <88736923+YanaZhekova@users.noreply.github.com>
2024-06-03 20:15:37 +0300

    Add files via upload

--- new_house.py
+++ new_house.py
@@ -7,31 +7,31 @@ total_price = 0.0
 
 if flowers == 'Roses':
     price = 5
-    if count >= 80:
+    if count > 80:
         total_price += (price * count - (price * count * 0.1))
     else:
         total_price = price * count
 elif flowers == 'Dahlias':
     price = 3.80
-    if count >= 90:
+    if count > 90:
         total_price += (price * count - (price * count * 0.15))
     else:
         total_price = price * count
 elif flowers == 'Tulips':
     price = 2.80
-    if count >= 80:
+    if count > 80:
         total_price += (price * count - (price * count * 0.15))
     else:
         total_price = price * count
 elif flowers == 'Narcissus':
     price = 3
-    if count <= 120:
+    if count < 120:
         total_price += (price * count + (price * count * 0.15))
     else:
         total_price = price * count
 elif flowers == 'Gladiolus':
     price = 2.5
-    if count <= 80:
+    if count < 80:
         total_price += (price * count + (price * count * 0.20))
     else:
         total_price = price * count
